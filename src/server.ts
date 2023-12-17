cc8b97e6cfc7f9adf0c9ab8ae8479b9008d3c49f
devlopersabbir <devlopersabbir@gmail.com>
2023-12-17 16:38:33 +0600

    [feat 🦾] string & error message validator

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import SendMail from "./sendMailer";
 dotenv.config();
 
 const app = express();
-app.get("/send", (req: Request, res: Response) => {
+app.post("/api/v1/mail/send", (req: Request, res: Response) => {
+  const { name, email, message } = req.body;
   new SendMail("mdsadikulislamsabbir@gmail.com", "Hello Sabbir", "Sabbir");
   res.status(200).json({
     hello: "world",
